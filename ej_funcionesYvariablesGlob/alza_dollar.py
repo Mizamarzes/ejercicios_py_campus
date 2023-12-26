0136ddfd742d0fd568953d93444b0c5c593cfacc
JuanContreras <mizamarzes@gmail.com>
2023-12-25 23:36:59 -0500

    .

--- ej_funcionesYvariablesGlob/alza_dollar.py
+++ ej_funcionesYvariablesGlob/alza_dollar.py
@@ -1,5 +1,29 @@
 #alza del dollar
+def alza(days):
+    maxAlza=0
+    temp=0
+    cont=0
+    for i in range(0,days,1):
+        cont+=1
+        print("Dia ",cont,":")
+        price=float(input())
 
+        if cont > 1:
+            dif = price - temp
+
+            if dif > maxAlza:
+                maxAlza = dif
+        temp = price
+    return maxAlza
+  
+days=int(input("Ingrese la cantidad de dias: "))
+result=alza(days)
+if result>0:
+    print("La mayor alza fue de: ", round(result, 2), " pesos")
+else:
+    print("Incorrecto")   
+        
+        
 
 
 
---
+++ ej_funcionesYvariablesGlob/maquinaAliments.py
@@ -0,0 +1,4 @@
+print("BIENVENIDOS")
+print("PRODUCTO A: $270")
+print("PRODUCTO B: $340")
+print("PRODUCTO C: $390")
