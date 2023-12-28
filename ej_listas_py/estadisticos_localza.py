9403ca92cc970a37689bc56ee194dee92aece211
JuanContreras <mizamarzes@gmail.com>
2023-12-27 21:59:58 -0500

    .

---
+++ ej_listas_py/estadisticos_localza.py
@@ -0,0 +1,26 @@
+#media aritmetica, armonica, mediana, moda
+
+def arithmetic_means(lista):
+    sum_list=sum(lista)
+    average=sum_list/len(lista)
+
+    return average
+
+list_num=([6, 1, 4, 8])
+resultado=arithmetic_means(list_num)
+print(resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ practicas_varias/funciones_for.py
@@ -0,0 +1,34 @@
+# def mi_funcion(num1,num2):
+#     sum=num1+num2
+#     return sum
+#
+# resultado=mi_funcion(3,4)
+# print(resultado)
+
+
+
+# cycle for
+# for i in [1,2.34,True,"hola","holag",24]:
+#     print(f"Elementos: {i}")
+
+
+
+# Dictionary, in this code box, I will explain how the dictionary is used with a structure for
+# colection={"Alejandro":23,"Maria":32,"Juanito":45,"Aleja":65}
+#
+# for i in colection:
+#     print(f"{colection[i]}")
+#
+# for clave,valor in colection.items():
+#     print(f"{clave} -> {valor}")
+
+
+
+# colection="JuanDiego"
+# for i in colection:
+#     print(f"{i}",end=" ")
+
+
+
+for i in range(0,5,1):
+    print(f"element: {i}")
